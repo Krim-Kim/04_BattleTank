61450af5176e0e56ebb6b60bb07403746d4fc8ea
Hemma\Kim <89kim.sjoberg@gmail.com>
2017-08-24 14:02:59 +0200

    L124

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto controlledTank = GetControlledTank();
+
+	if(controlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Tank Possessed! = %s"), *(controlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("NO AI Tank Possessed!"));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto controlledTank = GetControlledTank();
+
+	if(controlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Tank Possessed! = %s"), *(controlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("NO Player Tank Possessed!"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
